1cb56c39b8b3e670f697fb2a7d593a8e49064c00
Gustavo Henrique de Castilho <gustavo.castilho@audsat.com.br>
2024-01-18 13:16:15 -0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+// cypress.config.js
+const { defineConfig } = require('cypress')
+const cypressSplit = require('cypress-split')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      cypressSplit(on, config)
+      return config
+    },
+  },
+})
